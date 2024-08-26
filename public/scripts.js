af92857f7c76436c8f1322200a27c57caae5b342
guillaumeMischler <guillaume.mischler@zohomail.eu>
2024-08-26 21:30:46 +0200

    Initial commit with project files

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('maintenanceForm');
 
     form.addEventListener('submit', async function(event) {
@@ -27,6 +27,46 @@ document.addEventListener('DOMContentLoaded', function() {
             const result = await response.json();
 
             if (result.success) {
+                alert('Données soumises avec succès !');
+                form.reset(); // Réinitialiser le formulaire après soumission réussie
+            } else {
+                alert('Erreur lors de la soumission : ' + result.message);
+            }
+        } catch (error) {
+            console.error('Erreur:', error);
+            alert('Erreur lors de la soumission. Veuillez réessayer.');
+        }
+    });
+});*/
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('maintenanceForm');
+
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault(); // Empêche la soumission du formulaire
+
+        // Récupérer les données du formulaire
+        const data = {
+            machine: document.getElementById('machine').value,
+            type_maintenance: document.querySelector('input[name="maintenance"]:checked').value,
+            arret_machine: document.querySelector('input[name="arret"]:checked').value,
+            debut: document.getElementById('start').value,
+            fin: document.getElementById('end').value,
+            technicien: document.getElementById('technician').value,
+            notes: document.getElementById('notes').value
+        };
+
+        try {
+            const response = await fetch('https://script.google.com/macros/s/AKfycby0kV_2dqU6qR_UcvSqpk4m6u8xRnpcowvUVqkVE92lfUbWgdFWWNvNrB0tNI2dca97nw/exec', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams(data)
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
                 alert('Données soumises avec succès !');
                 form.reset(); // Réinitialiser le formulaire après soumission réussie
             } else {
